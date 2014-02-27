c479679a22fdded4325c35710e527ce7a311b48c
Debondt Didier <p@henix.be>
2014-02-27 18:23:25 +0100

    Script de mise à jour du mac

---
+++ update_mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Mise à jour des formules brew
+brew update
+# Mise à jour des prpgrammes installé avec Brew
+brew upgrade
+
+# Gem ruby
+sudo gem update
+
+# gem système ?
+sudo gem update --system

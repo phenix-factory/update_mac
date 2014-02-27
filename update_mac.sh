610693ddebd53b61e9910051c668523e7e83d70a
Debondt Didier <p@henix.be>
2014-02-27 19:49:52 +0100

    Réorganisation

--- update_mac.sh
+++ update_mac.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Mise à jour des formules brew
+echo 'Mise à jour des formules brew'
 brew update
 # Mise à jour des prpgrammes installé avec Brew
+echo 'Mise à jour des brew installées'
 brew upgrade
 
-# Gem ruby
-sudo gem update
-
 # gem système ?
+echo 'Mise à jour des gems système'
 sudo gem update --system
+
+# Gem ruby
+echo 'Mise à jour des gem'
+sudo gem update
